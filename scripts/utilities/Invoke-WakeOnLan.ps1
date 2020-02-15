fe87acfd72cfd3de5aa85de0605653f8c88f2753
Jason Scott <jscot2@outlook.com>
2020-02-15 11:18:24 -0800

    cleanup + feature add + remove status dialog

--- scripts/utilities/Invoke-WakeOnLan.ps1
+++ scripts/utilities/Invoke-WakeOnLan.ps1
@@ -16,40 +16,106 @@ param(
 
     [Parameter(Mandatory = $false)]
     [Switch]
-    $Wait
+    $Wait = $true
 )
 
-$MacByteArray = $TargetMacAddress -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
+#
+# Variables
+#
 
-[Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray  * 16)
+#
+# Helper methods
+#
 
-$UdpClient = New-Object System.Net.Sockets.UdpClient
+function Invoke-WOL() {
+    $MacByteArray = $TargetMacAddress -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
 
-$UdpClient.Connect(($GateWayIpAddress), 7)
+    [Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray  * 16)
 
-$ignore = $UdpClient.Send($MagicPacket, $MagicPacket.Length)
+    $UdpClient = New-Object System.Net.Sockets.UdpClient
 
-$UdpClient.Close()
+    $UdpClient.Connect(($GateWayIpAddress), 7)
 
-if ($Wait.IsPresent) {
-    if ([string]::IsNullOrEmpty($TargetIpAddress)) {
-        Write-Host "Wait was requested but target ip address is not present"
-    } else {
-        $progressPreference = 'silentlyContinue'
-        $connectionTestResult = $(Test-NetConnection -ComputerName $TargetIpAddress -InformationLevel Quiet -WarningAction SilentlyContinue) 
+    $ignore = $UdpClient.Send($MagicPacket, $MagicPacket.Length)
 
-        for ($i = 0; $i -lt 12; $i++) {
-            
-            if ($true -eq $connectionTestResult) {
-                Write-Host "Target machine is online"
-                break
-            } else {
-                Write-Host "Target machine is not online yet, waiting 10 seconds and trying again..."
-                Sleep 10
+    $UdpClient.Close()
+}
+
+function Test-TargetIsOnline() {
+    $ProgressPreference = 'SilentlyContinue'
+    $result = Test-NetConnection -ComputerName $TargetIpAddress -InformationLevel Quiet -WarningAction SilentlyContinue
+    $ProgressPreference = 'Continue'
+
+    return $result
+}
+
+function Write-TargetIsOnlineResult() {
+    $result = Test-TargetIsOnline
+    Write-Host "Target IP Address: $TargetIpAddress is currently online: $result"
+}
+
+function Get-IpAddressFromMacAddress() {
+    $formattedMacAddress = $TargetMacAddress
+
+    if ($formattedMacAddress.Contains(":")) {
+        $formattedMacAddress = $formattedMacAddress.Replace(":", "-")
+    }
+
+    $ipAddress = ""
+
+    try {
+        #$ipAddress = $(arp -a | select-string $formattedMacAddress |% { $_.ToString().Trim().Split(" ")[0] })
+        $ipAddress = $(Get-NetNeighbor -LinkLayerAddress "$formattedMacAddress").IPAddress
+
+        if (![string]::IsNullOrWhitespace($ipAddress)) {
+            Write-Host "Target IP Address was found from Mac Address: $ipAddress"
+        } else {
+            Write-Warning "Target IP Address was not found for Mac Address: $formattedMacAddress"
+        }
+    } catch {
+        Write-Error "Failed getting IP Address for Mac Address"
+    }
+
+    return $ipAddress
+}
+
+function Set-TargetIpAddressIfNotPresent() {
+    if ([string]::IsNullOrWhitespace($TargetIpAddress)) {
+        $ipForMacAddress = Get-IpAddressFromMacAddress
+        Set-Variable -Name "TargetIpAddress" -Value "$ipForMacAddress" -scope 1 -Force #1 == parent scope
+    }
+}
+
+function Wait-ForTargetOnline() {
+    if ($Wait.IsPresent) {
+        if ([string]::IsNullOrWhitespace($TargetIpAddress)) {
+            Write-Error "Wait was requested but target ip address is not present"
+            exit -1
+        } else {
+            $connectionTestResult = Test-TargetIsOnline
+    
+            for ($i = 0; $i -lt 12; $i++) {
                 
-                $connectionTestResult = $(Test-NetConnection -ComputerName $TargetIpAddress -InformationLevel Quiet -WarningAction SilentlyContinue) 
-                $ProgressPreference = "Continue"
+                if ($true -eq $connectionTestResult) {
+                    Write-Host "Target machine is online"
+                    break
+                } else {
+                    Write-Host "Target machine is not online yet, waiting 10 seconds and trying again..."
+                    Sleep 10
+                    
+                    $connectionTestResult = Test-TargetIsOnline
+                }
             }
         }
     }
 }
+
+#
+# Do Stuff
+#
+Invoke-WOL
+Set-TargetIpAddressIfNotPresent
+Write-TargetIsOnlineResult
+Wait-ForTargetOnline
+
+
