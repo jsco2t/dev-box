bd1f31399646cc4e01918cc45e6cf28c7092d898
Jason Scott <jscot2@outlook.com>
2021-05-28 16:11:50 -0600

    Fixing coffeetime wrapper to pass on arguments

--- windows/shells/pwsh/bin/coffeetime.ps1
+++ windows/shells/pwsh/bin/coffeetime.ps1
@@ -1,4 +1,5 @@
 # using an admin CMD session create a link to `coffeetime.csx` (in ~/bin), leave this script in the same folder (~/bin):
 #   mklink coffeetime.csx C:\Users\jason\Developer\Sources\personal\dev-box\windows\shells\pwsh\bin\coffeetime.csx
 
-dotnet-script "$ENV:HOME/bin/coffeetime.csx"
+& dotnet-script "$ENV:HOME/bin/coffeetime.csx" $args
+#dotnet-script "$ENV:HOME/bin/coffeetime.csx"
